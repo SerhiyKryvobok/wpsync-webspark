d411c9ddcc8167a0349dccb853a843d89d7d8fd4
Serhiy Kryvobok <serhiy.kryvobok@gmail.com>
2023-01-23 06:13:58 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # wpsync-webspark
+== Version: 1.0 ==
+
+# Warning! Restricted functionality, updates pending...
+
+== Description ==
+
+ - Plugin prepared specially for Webspark. For test task purposes.
+
+== Installation ==
+
+ - Just open the box!
+
+== Frequently Asked Questions ==
--- wpsync-webspark.php
+++ wpsync-webspark.php
@@ -1,113 +1,36 @@
 <?php
 /*
 Plugin Name:  wpsync-webspark
-Plugin URI:    
+Plugin URI:   https://github.com/SerhiyKryvobok/wpsync-webspark 
 Description:  Test task for Webspark 
 Version:      1.0
 Author:       SK 
-Author URI:   
-License:      
-License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  wpsynk
+Author URI:   https://github.com/SerhiyKryvobok
+License:      GNU General Public License v3.0
+License URI:  https://www.gnu.org/licenses/gpl-3.0.html
+Text Domain:  wpsync
 Domain Path:  /languages
 */
 	
-	if (!defined( 'ABSPATH')) {
-		exit;
+	if (!defined('ABSPATH')) exit;
+	
+	//if (!defined('EMPTY_TRASH_DAYS')) define( 'EMPTY_TRASH_DAYS', 0 );
+
+	require_once('sync.php');
+	
+	function wssk_activation() {
+		if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' )))) {
+			wssw_showmsg("Please install woocommerce!");
+			return;
+		}
+		flush_rewrite_rules();
+
 	}
+	register_activation_hook(__FILE__, 'wssk_activation');
 	
-	if (!in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' )))) {
-		exit;
-	} 
-	
-	add_action('wssk_synkhook', 'wssk_products', 10);
-	
-	function wssk_products() {
-		
-		$request = wp_remote_get( 'https://wp.webspark.dev/wp-api/products', ['timeout' => 20] );
-		$get_skus = [];
-		
-		if (is_wp_error($request)) {
-/*!!!*/			echo "Error!";
-			return false;
-		}
-		
-		$body = json_decode(wp_remote_retrieve_body( $request ));
-		
-		if ((!empty($body))&&(is_array($body->data))) {
-			echo "Items: " . count($body->data) . "<br />";
-			
-// Unquote before sending!!!
-			/*foreach ($body->data as $item) {
-				$get_skus[] = $item->sku;
-			}*/
-			
-			// Only for testing-----
-			for($i = 0; $i < 10; $i++) {
-				$get_skus[] = $body->data[$i]->sku;			
-			}
-//-----
-			
-		}
-		
-// ==================
-		
-		$products = wc_get_products( ['limit' => -1] );
-		$products_skus = [];
-		
-		foreach ($products as $item) {
-			$products_skus[$item->id] = $item->sku;
-		}
-		
-		$toWrite_skus = array_diff($get_skus, $products_skus);
-		$toUpdate_skus = array_intersect($products_skus, $get_skus);
-		$toDelete_skus = array_diff($products_skus, $get_skus);
-		
-		if (!empty( $toWrite_skus ) && array_unique( $toWrite_skus )) {
-			foreach ($toWrite_skus as $key => $val) {
-				$product = new WC_Product_Simple();
-				$product->set_sku($val);
-				$product->set_name($body->data[$key]->name);
-				$product->set_description($body->data[$key]->description);
-				$product->set_regular_price($body->data[$key]->price);
-				$product->set_stock_quantity($body->data[$key]->in_stock);
-				$product->save();
-			}
-		}
-		
-		/*if (!empty( $toUpdate_skus )) {
-			foreach ($toUpdate_skus as $product_key => $product_val) {
-				$product = wc_get_product($product_key);
-				foreach ($get_skus as $request_key => $request_val) {
-					if ($request_val === $product_val) {
-						//$product->set_sku($body->data[$request_key]->sku);
-						$product->set_name($body->data[$request_key]->name);
-						$product->set_short_description($body->data[$request_key]->description);
-						$product->set_price($body->data[$request_key]->price);
-						$product->set_stock_quantity($body->data[$request_key]->in_stock);
-						$product->save();
-					}
-				}
-			}
-		}*/
-		
-		if (!empty( $toDelete_skus )) {
-			foreach ($toDelete_skus as $key => $val) {
-				$product = wc_get_product($key);
-				$product->delete();
-			}
-		}
-		
-		/*echo "<pre>";
-			print_r($get_skus);
-			print_r($products_skus);
-			print_r($toWrite_skus);
-			print_r($toDelete_skus);
-		echo "</pre>";*/
-    }
-	
-	if( ! wp_next_scheduled( 'wssk_synkhook' ) ) {
-			wp_schedule_event( time(), 'hourly', 'wssk_synkhook' );
-		}
+	function wssk_deactivation() {
+		flush_rewrite_rules();
+	}	
+	register_deactivation_hook(__FILE__, 'wssk_deactivation');	
 	
 ?>
